f94efe66283ae1ec2d64cac9ef0e32650408cd1e
prerana2005 <preranamp2005@gmail.com>
2025-08-22 23:59:19 +0530

    d3

---
+++ 01.StringsArrays/LC-334.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for(int i =0; i<n; ++i){
+            if(first>=nums[i]){
+                first = nums[i];
+            }
+            else if(second>=nums[i]){
+                second = nums[i];
+            }
+            else 
+                return true;
+        }
+        return false;
+    }
+};
