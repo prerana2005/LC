8b14c9b63283d3fa94736fb1fd385d8efabad4e8
prerana2005 <preranamp2005@gmail.com>
2025-09-07 19:04:50 +0530

    d14

---
+++ 09.BinaryTree/LC-1372.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxPath = 0;
+
+    void solve(TreeNode* root, int left, int right){
+        if(root==nullptr){
+            return;
+        }
+        maxPath = max({maxPath,left,right});
+        solve(root->left,right+1,0);
+        solve(root->right,0,left+1);
+    }
+
+    int longestZigZag(TreeNode* root) {
+        solve(root,0,0);
+        return maxPath;
+    }
+};
