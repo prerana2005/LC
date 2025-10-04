0fa0e9968be1d0504645dc6a4cad143c94c9b07d
prerana2005 <preranamp2005@gmail.com>
2025-10-04 23:57:18 +0530

    d22

---
+++ 15.DP/LC-72.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+    int n = word1.size();
+    int m = word2.size();
+
+    // Create two arrays to store previous and current row of edit distances
+    vector<int> prev(m + 1, 0);
+    vector<int> cur(m + 1, 0);
+
+    // Initialize the first row
+    for (int j = 0; j <= m; j++) {
+        prev[j] = j;
+    }
+    if(n == 0) return prev[m]; // handle empty word1
+
+    // Calculate edit distances row by row
+    for (int i = 1; i <= n; i++) {
+        cur[0] = i; // Initialize the first column of the current row
+        for (int j = 1; j <= m; j++) {
+            if (word1[i - 1] == word2[j - 1]) {
+                // If the characters match, no additional cost
+                cur[j] = prev[j - 1];
+            } else {
+                // Minimum of three choices:
+                // 1. Replace the character at S1[i-1] with S2[j-1]
+                // 2. Delete the character at S1[i-1]
+                // 3. Insert the character at S2[j-1] into S1
+                cur[j] = 1 + min(prev[j - 1], min(prev[j], cur[j - 1]));
+            }
+        }
+        prev = cur; // Update the previous row with the current row
+    }
+
+    // The value at cur[m] contains the edit distance
+    return cur[m];
+    }
+};
